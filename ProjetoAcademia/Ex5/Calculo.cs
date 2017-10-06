f179171b9ac2dbc97640b2ea022c731fea823fda
Bruno <Bruno@DANTAS-PC>
2017-10-05 23:32:45 -0300

    Ex5.1 alteração

---
+++ ProjetoAcademia/Ex5/Calculo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    class Calculo
+    {
+        public decimal ObtenhaMontante(decimal capitalInicial, decimal taxaJuro, decimal prazo)
+        {
+            //Código a ser implementado
+            decimal FV =0;
+
+
+            for (int aux = 0; aux <= prazo; aux++)
+            {
+                FV = capitalInicial * (1 + taxaJuro) * aux;
+
+            }
+
+            //FV = capitalInicial * ( taxaJuro prazo));
+
+
+            Console.WriteLine("Montante com juros aplicado : " + FV);
+            Console.ReadKey();
+            return FV;
+
+        }
+    }
+}
--- ProjetoAcademia/Ex5/Ex5.cs
+++ ProjetoAcademia/Ex5/Ex5.cs
@@ -8,35 +8,41 @@ namespace Ex5
 {
     class Ex5
     {
-        decimal capitalInicial = 10000; //PV
-        decimal taxaJuro = 3 / 100 * 10000; //i- TAXA DE JUROS
-        decimal prazo = 3; //n - PERIODOS DE CAPTALIZAÇÃO
-        decimal FV = 0; //MONTANTE
+      
 
         static void Main(string[] args)
         {
-            
+            decimal capitalInicial = 10000; //PV
+            decimal taxaJuro = 3 / 100 * 10000; //i- TAXA DE JUROS
+            decimal prazo = 3; //n - PERIODOS DE CAPTALIZAÇÃO
+            //decimal FV = 0; //MONTANTE
+
+            Calculo calc = new Calculo();
+
+            calc.ObtenhaMontante(capitalInicial, taxaJuro, prazo);
 
         }
         
 
-        public decimal ObtenhaMontante(decimal capitalInicial, decimal taxaJuro, decimal prazo)
-        {
-            //Código a ser implementado
+        //public decimal ObtenhaMontante(decimal capitalInicial, decimal taxaJuro, decimal prazo)
+        //{
+        //    //Código a ser implementado
             
-            for (int aux = 0; aux <= prazo;aux++)
-            {
-                FV = capitalInicial*(1 + taxaJuro)*aux;
+
+            
+        //    for (int aux = 0; aux <= prazo;aux++)
+        //    {
+        //        FV = capitalInicial*(1 + taxaJuro)*aux;
                 
-            }
+        //    }
 
-            //FV = capitalInicial * ( taxaJuro prazo));
+        //    //FV = capitalInicial * ( taxaJuro prazo));
 
 
-            Console.WriteLine("Montante com juros aplicado : "+FV);
-            Console.ReadKey();
-            return FV;
+        //    Console.WriteLine("Montante com juros aplicado : "+FV);
+        //    Console.ReadKey();
+        //    return FV;
 
-        }
+        //}
     }
 }
