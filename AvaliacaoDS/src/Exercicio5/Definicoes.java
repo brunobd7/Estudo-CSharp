09c879721731c42d88c3ab2ab4634c20633a0765
Bruno <Bruno@DANTAS-PC>
2017-10-05 22:36:18 -0300

    Ex5.1

---
+++ AvaliacaoDS/src/Exercicio5/Definicoes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicio5;
+
+/**
+ *
+// * @author Bruno
+ */
+public class Definicoes {
+    
+      
+    
+    public double ObetenhaMontante(double capitalInicial, double taxaJuro , double prazo){
+        double montante=capitalInicial;
+        
+        montante = 
+        //montante = capitalInicial*(Math.pow((1+taxaJuro),prazo));
+        
+        System.out.println("Montante pós aplicação dos juros : "+montante);
+        return montante;  
+        
+    }
+   
+}
---
+++ ProjetoAcademia/Ex5/Ex5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    class Ex5
+    {
+        decimal capitalInicial = 10000; //PV
+        decimal taxaJuro = 3 / 100 * 10000; //i- TAXA DE JUROS
+        decimal prazo = 3; //n - PERIODOS DE CAPTALIZAÇÃO
+        decimal FV = 0; //MONTANTE
+
+        static void Main(string[] args)
+        {
+            
+
+        }
+        
+
+        public decimal ObtenhaMontante(decimal capitalInicial, decimal taxaJuro, decimal prazo)
+        {
+            //Código a ser implementado
+            
+            for (int aux = 0; aux <= prazo;aux++)
+            {
+                FV = capitalInicial*(1 + taxaJuro)*aux;
+                
+            }
+
+            //FV = capitalInicial * ( taxaJuro prazo));
+
+
+            Console.WriteLine("Montante com juros aplicado : "+FV);
+            Console.ReadKey();
+            return FV;
+
+        }
+    }
+}
